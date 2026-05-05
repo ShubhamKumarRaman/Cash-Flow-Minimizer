328f02c6e3cc22cf6bc6b5ec781d760e73d2effd
Shubham Kumar <shubham620409@gmail.com>
2026-05-05 20:27:05 +0530

    Create user.routes.js

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { getUsers } from '../controllers/user.controller'
+import { protect } from '../middlewares/auth.middleware'
+
+const router = express.Router();
+
+router.get('/', protect, getUsers);
+
+export default router;
