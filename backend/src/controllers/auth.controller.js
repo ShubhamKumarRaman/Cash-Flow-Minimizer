b25e8526c1ca61fe24fa5f4fd740cca8f6d364bd
Shubham Kumar <shubham620409@gmail.com>
2026-05-05 20:03:50 +0530

    Create auth.controller.js

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,44 @@
+import User from '../models/user.model'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+export const register = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = await User.create(
+        {
+            name,
+            email,
+            password: hashed
+        }
+    )
+
+    res.json(user);
+};
+
+
+export const login = async (req, res) => {
+    const user = await User.findOne({ email: req.body.email })
+
+    if (!user) {
+        return res.status(404).json("User not found");
+    }
+
+    const valid = await bcrypt.compare(
+        req.body.password,
+        user.password
+    )
+
+    if (!valid) {
+        return res.status(400).json("Invalid credentials");
+    }
+
+    const token = jwt.sign(
+        { id: user._id },
+        process.env.JWT_SECRET
+    )
+
+    res.json({ token });
+}
