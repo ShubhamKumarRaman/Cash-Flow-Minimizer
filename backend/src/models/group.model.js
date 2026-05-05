42e4d3e6da19d96d8280b89f52567269b0a434bf
Shubham Kumar <shubham620409@gmail.com>
2026-05-05 19:28:11 +0530

    Create group.model.js

---
+++ backend/src/models/group.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const groupSchema = new mongoose.Schema(
+    {
+        name: String,
+        members: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ]
+    }, { timestamps: true }
+);
+
+export default mongoose.model("Group", groupSchema);
