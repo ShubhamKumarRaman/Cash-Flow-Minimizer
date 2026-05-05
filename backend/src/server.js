8eccb88b3303af404a16d25afb97307bbfa655f9
Shubham Kumar <shubham620409@gmail.com>
2026-05-05 20:39:06 +0530

    Create server.js

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import app from './app';
+import { connectDB } from './config/db';
+
+dotenv.config();
+
+connectDB();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on PORT: ${process.env.PORT}`);
+})
