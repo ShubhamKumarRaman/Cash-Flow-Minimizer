47c3a090a656cdb3a05df81928864e7f5fc1bff0
Shubham Kumar <shubham620409@gmail.com>
2026-05-05 20:19:45 +0530

    Create group.controller.js

---
+++ backend/src/controllers/group.controller.js
@@ -0,0 +1,18 @@
+import Group from "../models/group.model";
+
+export const createGroup = async (req, res) => {
+    const { name, members } = req.body;
+
+    const group = await Group.create({
+        name,
+        members,
+    });
+
+    res.json(group);
+}
+
+export const getGroups = async (req, res) => {
+    const groups = await Group.find().populate("members", "name email");
+
+    res.json(groups);
+}
