a02905e4dba8c6db21144f189e76c6a90266476e
Shubham Kumar <shubham620409@gmail.com>
2026-05-06 13:52:45 +0530

    Update settlement.routes.js

--- backend/src/routes/settlement.routes.js
+++ backend/src/routes/settlement.routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getSettlement } from '../controllers/settlement.controller.js';
+import { getBalances, getSettlement } from '../controllers/settlement.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
-router.get('/:groupId', protect, getBalances);
+router.get('/balances/:groupId', protect, getBalances);
+router.get('/optimize/:groupId', protect, getSettlement);
 
 export default router;
