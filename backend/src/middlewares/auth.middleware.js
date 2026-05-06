b0dbb066e0861ae8d2a70761fc3014c34ced34ea
Shubham Kumar <shubham620409@gmail.com>
2026-05-06 10:35:16 +0530

    Add bearer token check point

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,16 +1,20 @@
 import jwt from 'jsonwebtoken'
 
 export const protect = (req, res, next) => {
-    const token = req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
-    if (!token)
-        return res.status(401).json("No Token");
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+        return res.status(401).json({message:"No token provided"})
+    }
 
+    const token = authHeader.split(" ")[1];
+
+    
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.id;
         next();
     } catch (error) {
-        res.status(401).json("Invalid token");
+        res.status(401).json({message:"Invalid token"});
     }
 }
