3c8f36b5915dd6a965ee427960118bfbf1ed45e6
Shubham Kumar <shubham620409@gmail.com>
2026-05-07 11:39:37 +0530

    Update expense.controller.js

--- backend/src/controllers/expense.controller.js
+++ backend/src/controllers/expense.controller.js
@@ -1,4 +1,5 @@
 import Expense from '../models/expense.model.js'
+import { APIFeatures } from '../utils/apiFeatures.js';
 
 export const addExpense = async (req, res) => {
     const { groupId, paidBy, amount, members } = req.body;
@@ -19,10 +20,21 @@ export const addExpense = async (req, res) => {
     res.json(expense);
 }
 
-export const getExpenses = async (req, res) => {
-    const expenses = await Expense.find({
-        groupId: req.params.groupId,
-    }).populate("paidBy", "name");
+// export const getExpenses = async (req, res) => {
+//     const expenses = await Expense.find({
+//         groupId: req.params.groupId,
+//     }).populate("paidBy", "name");
 
-    res.json(expenses);
+//     res.json(expenses);
+// }
+
+export const getExpenses = async (req, res) => {
+    const featuers = new APIFeatures(
+        Expense.find({ groupId: req.params.groupId }),
+        req.query
+    ).paginate();
+
+    const expenses = await featuers.query;
+
+    res.json(expense);
 }
