0719e3707606d03fefc18fecdc4ece60c4e509d1
Shubham Kumar <shubham620409@gmail.com>
2026-05-07 10:11:08 +0530

    Create export.routes.js

---
+++ backend/src/routes/export.routes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { exportReport } from '../controllers/export.controller.js'
+import { protect } from '../middlewares/auth.middleware.js'
+
+const router = express.Router();
+
+router.get('/:gruopId', protect, exportReport);
+
+export default router;
