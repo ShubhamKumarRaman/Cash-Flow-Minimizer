8fc220c962bbe08ef2ccde506043adde64e787db
Shubham Kumar <shubham620409@gmail.com>
2026-05-07 07:46:27 +0530

    Create Login.jsx

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,27 @@
+import { useState, useContext } from "react";
+import API from "../api/axios";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from 'react-router-dom';
+
+function login() {
+    const [form, setForm] = useState({ email: "", password: "" });
+    const { login } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleSubmit = async () => {
+        const res = await API.post('/auth/login', form);
+        login(res.data.token);
+        navigate('/dashboard');
+    }
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <input type="text" placeholder="Email" onChange={e => setForm({ ...form, email: e.target.value })} />
+            <input type="text" placeholder="Password" type="password" onChange={e => setForm({ ...form, password: e.target.value })} />
+            <button onClick={handleSubmit}>Login</button>
+        </div>
+    )
+}
+
+export default login;
