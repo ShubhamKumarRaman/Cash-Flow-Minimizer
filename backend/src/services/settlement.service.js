1a67ad3aae5e7d91a7eec160ec46fae631d1b3ec
Shubham Kumar <shubham620409@gmail.com>
2026-05-07 11:23:20 +0530

    add settlement service

--- backend/src/services/settlement.service.js
+++ backend/src/services/settlement.service.js
@@ -1,5 +1,6 @@
 import { calculateBalances } from "./balance.service.js";
 import { minimizeCashFlow } from "../algorithms/greedyMinimizer.js";
+import {Settlement} from '../models/settlement.model.js'
 
 export const getOptimizedSettlement = async (groupId) => {
     const balanceMap = await calculateBalances(groupId);
@@ -7,4 +8,19 @@ export const getOptimizedSettlement = async (groupId) => {
     const transactions = minimizeCashFlow(balanceMap);
 
     return transactions;
+}
+
+export const saveSettlements = async (groupId, transactions) => {
+    await Settlement.deleteMany({ groupId });
+
+    const data = transactions.map((t) => ({
+        groupId,
+        from: t.from,
+        to: t.to,
+        amount: t.amount,
+        dueDate: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        interestRate: 0,
+    }))
+
+    return Settlement.insertMany(data);
 }
